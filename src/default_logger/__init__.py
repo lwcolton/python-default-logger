21453ab2d03e1655b5c24bfef571ee00075714c6
Colton Leekley-Winslow <colton@hurricanelabs.com>
2016-04-07 18:28:38 -0500

    initial commit

---
+++ src/default_logger/__init__.py
@@ -0,0 +1,12 @@
+import logging
+
+def get_default_logger(name, level=None):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.propogate = False
+    formatter =logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
